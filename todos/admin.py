17be1c68a0a393afb8a2d69cb290b9e03dc736d9
ak <akayela>
2024-12-26 12:18:42 +0200

    added modes to admin site

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import(
+    Task,
+    TaskStatusChange,
+    Why,
+    Answer,
+)
 # Register your models here.
+
+admin.site.register(Task)
+admin.site.register(TaskStatusChange)
+admin.site.register(Why)
+admin.site.register(Answer)
