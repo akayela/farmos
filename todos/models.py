a69b17864a198ed8ceab5285ebe7590b41b8f044
akayela <140437932+akayela@users.noreply.github.com>
2024-12-27 09:23:47 +0200

    Update models.py

--- todos/models.py
+++ todos/models.py
@@ -4,41 +4,50 @@ from django.utils import timezone
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    status = models.CharField(max_length=20, choices=[
-        ('complete', 'Complete'),
-        ('pending', 'Pending'),
-        ('in_progress', 'In Progress'),
-        ('incomplete', 'Incomplete')
-    ])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     point = models.CharField(max_length=100)
     due = models.DateField()
+    
+    class Meta:
+        verbose_name = "Task"
 
     def __str__(self):
         return self.title
 
-class TaskStatusChange(models.Model):
+class Status(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    previous_status = models.CharField(max_length=20)
-    current_status = models.CharField(max_length=20)
-    changed_at = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=20, choices=[
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('complete', 'Complete'),
+        ('incomplete', 'Incomplete')
+    ])
+    
+    class Meta:
+        verbose_name = "Status"
 
     def __str__(self):
-        return self.current_status
+        return self.status
 
-class Why(models.Model):
-    task = models.ForeignKey(Task, related_name='whys', on_delete=models.CASCADE)
-    question = models.CharField(max_length=255)
+class Analysis(models.Model):
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    question_1 = models.CharField(max_length=255)
+    answer_1 = models.TextField()
+    question_2 = models.CharField(max_length=255)
+    answer_2 = models.TextField()
+    question_3 = models.CharField(max_length=255)
+    answer_3 = models.TextField()
+    question_4 = models.CharField(max_length=255)
+    answer_4 = models.TextField()
+    question_5 = models.CharField(max_length=255)
+    answer_5 = models.TextField()
+    root_cause = models.TextField()
+    solution = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.question
-
-class Answer(models.Model):
-    why = models.ForeignKey(Why, on_delete=models.CASCADE)
-    answer = models.TextField()
-    given_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        verbose_name = "Analysis"
 
     def __str__(self):
-        return self.answer
+        return self.solution
